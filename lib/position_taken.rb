746b1ac0fbf896c63e1dc8f561c3165c24621471
James Daaran <jamesdaaran@gmail.com>
2021-04-01 14:09:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@ board = [1,2,3,4," ", " ", " "]
 value = gets.strip
 index = value - 1
 position_taken(board,index)
-  if board[index] == "" or " "
+  if board[index] == 0
     true
   else
     false
